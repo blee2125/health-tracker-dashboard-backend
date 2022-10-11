0f47e2606d5d707b3b57cb24900f6b689cf3a92e
Brandon Lee <blee2125@gmail.com>
2022-10-11 14:18:48 -0600

    add search by date routes

--- routes/water.js
+++ routes/water.js
@@ -19,6 +19,34 @@ router.post('/post', async (req, res) => {
     }
 })
 
+//Get by date (time property) Method
+router.get('/searchByDate', async (req, res) => {
+    try{
+        const searchDate = 'Oct 10 2022' // update to accept external value
+        const waterData = await WaterModel.find({'time': { '$regex' : searchDate, '$options' : 'i' }});
+        res.json(waterData)
+        console.log(waterData)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by date (createdAt property) Method
+router.get('/getByDate', async (req, res) => {
+    try{
+        let start = new Date("2022-10-10"); // update to accept external value
+        let end = new Date("2022-10-11");
+        end.setUTCHours(23,59,59,999);
+        const waterData = await WaterModel.find({'createdAt': {$gte: start, $lt: end}});
+        res.json(waterData)
+        console.log(waterData)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
 //Get all Method
 router.get('/getAll', async (req, res) => {
     try{
