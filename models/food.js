b3a9818150f1d562445ff645db1be0e137c3e2e0
Brandon Lee <blee2125@gmail.com>
2022-10-14 16:59:16 -0600

    build food model

---
+++ models/food.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    foodName: {
+        required: true,
+        type: String
+    },
+    totalCalories: {
+        required: false,
+        type: Number
+    },
+    timeOfConsumption: {
+        required: false,
+        type: String
+    },
+    meal: {
+        required: false,
+        type: String
+    },    
+    placeOfConsumption: {
+        required: false,
+        type: String
+    },
+    withWhom: {
+        required: false,
+        type: String
+    },    
+    activity: {
+        required: false,
+        type: String
+    },
+    mood: {
+        required: false,
+        type: String
+    },    
+    hungerLevel: {
+        required: false,
+        type: String
+    },
+    fullness: {
+        required: false,
+        type: String
+    },    
+    amount: {
+        required: false,
+        type: String
+    },
+    time: {
+        required: true,
+        type: String,
+        default: () => { return new Date() }
+    }
+})
+
+dataSchema.set('timestamps', true);
+
+module.exports = mongoose.model('Food', dataSchema)
