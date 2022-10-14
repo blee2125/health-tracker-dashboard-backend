70d7a18dc6ba1232bd46679973505fc369690b41
Brandon Lee <blee2125@gmail.com>
2022-10-14 10:09:31 -0600

    add additional routes

--- routes/exercise.js
+++ routes/exercise.js
@@ -21,3 +21,58 @@ router.post('/post', async (req, res) => {
         res.status(500).json({message: error.message})
     }
 })
+
+//Get by date (time property) Method
+router.get('/searchByDate', async (req, res) => {
+    let searchDate = req._parsedUrl.query
+    let searchDate2 = searchDate.split('=')
+    let searchDateFinal = searchDate2[1].toString().split('+')
+    try{
+        const exerciseData = await ExerciseModel.find({'time': { '$regex' : `${searchDateFinal[0]} ${searchDateFinal[1]} ${searchDateFinal[2]}`, '$options' : 'i' }});
+        res.json(exerciseData)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    try{
+        const exerciseData = await ExerciseModel.find();
+        res.json(exerciseData)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const exerciseData = await ExerciseModel.findById(req.params.id);
+        res.json(exerciseData)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//Update by ID Method
+router.put('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await ExerciseModel.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
