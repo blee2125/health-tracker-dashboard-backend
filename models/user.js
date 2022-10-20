396a436e95eff90294fdbbd623c6763b3cb8aae3
Brandon Lee <blee2125@gmail.com>
2022-10-20 15:02:50 -0600

    create User model

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    }
+})
+
+dataSchema.set('timestamps', true);
+
+module.exports = mongoose.model('User', dataSchema)
