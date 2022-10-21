f6a6c95ac5908c881af500366ea5e3562f15b638
Brandon Lee <blee2125@gmail.com>
2022-10-21 14:11:04 -0600

    create auth check

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const token = req.header("x-auth-token");
+    if (!token) {
+        return res.status(401).json({msg: "No authentication token, access denied"});
+    }
+    try{
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) {
+            return res.status(401).json({msg: "Token verification failed, authorization denied"});
+        }
+        req.user = verified.id;
+        next();
+        console.log('verified')
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+module.exports = auth;
