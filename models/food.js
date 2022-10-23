10b6e37c552f06d4142898633fc9aad245276941
Brandon Lee <blee2125@gmail.com>
2022-10-23 13:59:45 -0600

    add userId to model

--- models/food.js
+++ models/food.js
@@ -49,6 +49,10 @@ const dataSchema = new mongoose.Schema({
         required: true,
         type: String,
         default: () => { return new Date() }
+    },
+    userId: {
+        type: String,
+        required: true
     }
 })
 
