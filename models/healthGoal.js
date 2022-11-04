9315d5a99fb54478477bacacd162c24a3408b6a5
Brandon Lee <blee2125@gmail.com>
2022-11-04 16:58:32 -0600

    create health goal model

---
+++ models/healthGoal.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    goal: {
+        required: true,
+        type: String
+    },
+    category: {
+        required: false,
+        type: String
+    },
+    timeframe: {
+        required: false,
+        type: String
+    },
+    endDate: {
+        required: false,
+        type: String
+    },
+    completed: {
+        required: true,
+        type: Boolean,
+        default: false
+    },
+    startDate: {
+        required: true,
+        type: String,
+        default: () => { return new Date() }
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+})
+
+dataSchema.set('timestamps', true);
+
+module.exports = mongoose.model('HealthGoal', dataSchema)
